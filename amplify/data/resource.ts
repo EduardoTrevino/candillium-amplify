8e9519281bd02a818badb890658fd263ee9f1c39
EduardoTrevino <realeddietrevino@gmail.com>
2024-10-05 20:18:28 -0400

    Fixed per user data isolation, and domain

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -8,10 +8,8 @@ specifies that any user authenticated via an API key can "create", "read",
 =========================================================================*/
 const schema = a.schema({
   Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
+    .model({content: a.string(),
+    }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,9 +18,6 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "userPool",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
   },
 });
 
